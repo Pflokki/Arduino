f43cc19139bcc98804cbe3f2d31218472dc58db8
Konstantin Uvarov <konstantin.uvar@gmail.com>
2017-08-26 02:48:33 +0700

    Оптимизация кода: 1. Убрал все упоминания о delay(), теперь задержка реализована через millis(). 2. Изменил реализацию кнопки, теперь ее можно повесить на любой пин, не использую прерывание. 3. Убрал использование встроенного светодиода.

--- rgb_led/rgb_led.ino
+++ rgb_led/rgb_led.ino
@@ -1,9 +1,24 @@
+/*
+	
+	9 - красный, 10 - зеленый, 11 - синий
+	2 - кнопка
+
+*/
+
 int pin_Red = 9;
 int pin_Green = 10;
 int pin_Blue = 11;
 
 int pin_Button = 2;
-int mode = 0;
+
+int current_mode = 0;
+
+bool f_first_enter = true;
+unsigned long time_old = 0;
+int h, s;
+int h_old, s_old;
+bool f_return = false;
+
 struct rgbColor {
   unsigned int r;
   unsigned int g;
@@ -70,150 +85,211 @@ void set_RGB (rgbColor rgb) {
 }
 
 // весь спектр
-void mode_1() {
-  int timeout = 100;
-  for (int i = 0; i < 360; i++) {
-    set_RGB(HUE_to_RGB(i, 100, 100));
-    delay(timeout);
-  }
+void mode_full_spectr() {
+	int timeout = 100;
+
+	if (f_first_enter) {
+		time_old = millis();
+		h = 0;
+		h_old = -1;
+		f_first_enter = false;
+	}
+	if (millis() - time_old  >= timeout) {
+		h++;
+		time_old = millis();
+	}
+	if (h > 360)
+		h = 0;
+	if (h != h_old) {
+		set_RGB(HUE_to_RGB(h, 100, 100));
+		h_old = h;
+	}
 }
 
-// красный, оранжевый, желтый, зеленый
-void mode_2() {
+// 0-140    красный, оранжевый, желтый, зеленый
+// 180-280  голубой, синий, фиолетоый
+// 280-360  фиолетовый, розовый, красный
+void mode_range(int h_min, int h_max) {
   int timeout = 100;
-  for (int i = 0; i < 140; i++) {
-    set_RGB(HUE_to_RGB(i, 100, 100));
-    delay(timeout);
-  }
-  for (int i = 140; i > 0; i--) {
-    set_RGB(HUE_to_RGB(i, 100, 100));
-    delay(timeout);
-  }
-}
 
-// голубой, синий, фиолетоый
-void mode_3() {
-  int timeout = 100;
-  for (int i = 180; i < 280; i++) {
-    set_RGB(HUE_to_RGB(i, 100, 100));
-    delay(timeout);
+  if (f_first_enter) {
+	  time_old = millis();
+	  h = h_min;
+	  h_old = -1;
+	  f_return = false;
+	  f_first_enter = false;
   }
-  for (int i = 280; i > 180; i--) {
-    set_RGB(HUE_to_RGB(i, 100, 100));
-    delay(timeout);
-  }
-}
 
-// фиолетовый, розовый, красный
-void mode_4() {
-  int timeout = 100;
-  for (int i = 280; i < 360; i++) {
-    set_RGB(HUE_to_RGB(i, 100, 100));
-    delay(timeout);
+  if (!f_return) {
+	  if (millis() - time_old  >= timeout) {
+		  h++;
+		  time_old = millis();
+	  }
+	  if (h >= h_max)
+		  f_return = !f_return;
+	  if (h != h_old) {
+		  set_RGB(HUE_to_RGB(h, 100, 100));
+		  h_old = h;
+	  }
   }
-  for (int i = 360; i > 280; i--) {
-    set_RGB(HUE_to_RGB(i, 100, 100));
-    delay(timeout);
+  else {
+	  if (millis() - time_old  >= timeout) {
+		  h--;
+		  time_old = millis();
+	  }
+	  if (h <= h_min)
+		  f_return = !f_return;
+	  if (h != h_old) {
+		  set_RGB(HUE_to_RGB(h, 100, 100));
+		  h_old = h;
+	  }
   }
 }
 
 // просмотр всех цветов
-void mode_5() {
-  int timeout = 1000;
-  set_RGB(RED);
-  delay(timeout);
-  set_RGB(ORANGE);
-  delay(timeout);
-  set_RGB(YELLOW);
-  delay(timeout);
-  set_RGB(GREEN);
-  delay(timeout);
-  set_RGB(BLUE);
-  delay(timeout);
-  set_RGB(VIOLET);
-  delay(timeout);
-  set_RGB(PINK);
-  delay(timeout);
-  set_RGB(WHITE);
-  delay(timeout * 4);
+void mode_full_color() {
+	int timeout = 1000;
+  
+	if (f_first_enter) {
+		time_old = millis();
+		f_first_enter = false;
+	}
+	if (millis() - time_old  >= 12 * timeout)
+		time_old = millis();
+	else if (millis() - time_old  >= 8 * timeout)
+		set_RGB(RED);
+	else if (millis() - time_old  >= 7 * timeout)
+		set_RGB(ORANGE);
+	else if (millis() - time_old  >= 6 * timeout)
+		set_RGB(YELLOW);
+	else if (millis() - time_old  >= 5 * timeout)
+		set_RGB(GREEN);
+	else if (millis() - time_old  >= 4 * timeout)
+		set_RGB(BLUE);
+	else if (millis() - time_old  >= 3 * timeout)
+		set_RGB(VIOLET);
+	else if (millis() - time_old  >= 2 * timeout)
+		set_RGB(PINK);
+	else if (millis() - time_old  >= timeout)
+		set_RGB(WHITE);
 }
 
 // закат
+
+// 1 step: from h = 30 s = 90  to h = 30 s = 100
+// 2 step: from h = 30 s = 100 to h = 15 s = 100
 void sunset() {
-  int v = 100;
   int y_timeout = 1000;
   int o_timeout = 2000;
-  int f_timeout = 20000;
-  // 1 step: from h = 30 s = 90  v = 100 to h = 30 s = 100 v = 100 with delay = 300
-  for (int s = 90; s <= 100; s++) {
-    set_RGB(HUE_to_RGB(30, s, v));
-    delay(y_timeout);
+
+  if (f_first_enter) {
+	  time_old = millis();
+	  h = 30;
+	  h_old = 31;
+	  s = 90;
+	  s_old = -1;
+	  f_first_enter = false;
   }
-  // 2 step: from h = 30 s = 100 v = 100 to h = 15 s = 100 v = 100 with delay = 500
-  for (int h = 30; h >= 5; h--) {
-      set_RGB(HUE_to_RGB(h, 100, v));
-      delay(o_timeout);
+  if (s != 100) {
+	  if (millis() - time_old  >= y_timeout) {
+		  s++;
+		  time_old = millis();
+	  }
+	  if (s != s_old) {
+		  set_RGB(HUE_to_RGB(30, s, 100));
+		  s_old = s;
+	  }
   }
-  delay(f_timeout);
+  else if (h != 5) {
+	  if (millis() - time_old  >= o_timeout) {
+		  h--;
+		  time_old = millis();
+	  }
+	  if (h != h_old) {
+		  set_RGB(HUE_to_RGB(h, 100, 100));
+		  h_old = h;
+	  }
+  }
 }
 
 // рассвет
+// 1 step: from h = 10 s = 100 to h = 30 s = 100
+// 2 step: from h = 30 s = 100 to h = 30 s = 90
 void sunrise() {
   int v = 100;
   int y_timeout = 1000;
   int o_timeout = 2000;
   int f_timeout = 20000;
-  // 1 step: from h = 15 s = 100 v = 100 to h = 30 s = 100 v = 100 with delay = 300
-  for (int h = 10; h <= 30; h++) {
-    set_RGB(HUE_to_RGB(h, 100, v));
-    delay(y_timeout);
+
+  if (f_first_enter) {
+	  time_old = millis();
+	  h = 10;
+	  h_old = 9;
+	  s = 100;
+	  s_old = 101;
+	  f_first_enter = false;
   }
-  // 2 step: from h = 30 s = 100 v = 100 to h = 30 s = 90 v = 100 with delay = 500
-  for (int s = 100; s >= 90; s--) {
-      set_RGB(HUE_to_RGB(30, s, v));
-      delay(o_timeout);
+
+  if (h != 30) {
+	  if (millis() - time_old  >= y_timeout) {
+		  h++;
+		  time_old = millis();
+	  }
+	  if (h != h_old) {
+		  set_RGB(HUE_to_RGB(h, 100, 100));
+		  h_old = h;
+	  }
   }
-  delay(f_timeout);
+  else if (s != 90) {
+	  if (millis() - time_old  >= o_timeout) {
+		  s--;
+		  time_old = millis();
+	  }
+	  if (s != s_old) {
+		  set_RGB(HUE_to_RGB(30, s, 100));
+		  s_old = s;
+	  }
+  }
 }
 
-void inc_Mode() {
-  mode++;
 
-  if (mode > 15) 
-    mode = 0;
-}
-    
 void setup() {
   pinMode(pin_Red, OUTPUT);
   pinMode(pin_Green, OUTPUT);
   pinMode(pin_Blue, OUTPUT);
 
   pinMode(pin_Button, INPUT_PULLUP);
-  //digitalWrite(pin_Button, HIGH);
-  attachInterrupt(0, inc_Mode, CHANGE);
-
-  mode_5();
+  digitalWrite(pin_Button, HIGH);
 }
 
+
 void loop() {
-  switch (mode) {
-    case 0: mode_1(); break;
-    case 1: mode_2(); break;
-    case 2: mode_3(); break;
-    case 3: mode_4(); break;
-    case 4: mode_5(); break;
 
-    case 5: set_RGB(RED); break;
-    case 6: set_RGB(ORANGE); break;
-    case 7: set_RGB(YELLOW); break;
-    case 8: set_RGB(GREEN); break;
-    case 9: set_RGB(BLUE); break;
-    case 10: set_RGB(VIOLET); break;
-    case 11: set_RGB(PINK); break;
-    case 12: set_RGB(WHITE); break;
-    case 13: mode_5(); break;
+	if (digitalRead(pin_Button) == LOW) {
+    delay(300);
+		current_mode++;
+		f_first_enter = true;
+	}
+	if (current_mode > 14)
+		current_mode = 0;
 
-    case 14: sunset(); break;
-    case 15: sunrise(); break;
+	switch (current_mode) {
+		case 0: mode_full_spectr(); break;
+		case 1: mode_range(0, 140); break;
+		case 2: mode_range(180, 280); break;
+		case 3: mode_range(280, 360); break;
+
+		case 4: set_RGB(RED); break;
+		case 5: set_RGB(ORANGE); break;
+		case 6: set_RGB(YELLOW); break;
+		case 7: set_RGB(GREEN); break;
+		case 8: set_RGB(BLUE); break;
+		case 9: set_RGB(VIOLET); break;
+		case 10: set_RGB(PINK); break;
+		case 11: set_RGB(WHITE); break;
+		case 12: mode_full_color(); break;
+
+		case 13: sunset(); break;
+		case 14: sunrise(); break;
   }
 }
